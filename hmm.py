c04c8709cc0cc2f735a6bdb96152260b9e1a67b6
Omar Yahia <therealrandomath@gmail.com>
2025-11-22 12:58:01 -0800

    hmm with ~38% winrate

--- hmm.py
+++ hmm.py
@@ -4,6 +4,7 @@ from src.environment.blackjack import BlackjackEnv
 import json
 import numpy as np
 from tqdm import tqdm
+import copy
 
 RAW_DATA_PATH = 'blackjack_data.json'
 
@@ -29,9 +30,9 @@ def card_to_index(card: str):
 def special_to_index(special: Literal['cont', 'win', 'lose', 'draw']):
     # maps special emissions to 52-54
     descs = ['cont', 'win', 'lose', 'draw']
-    return descs.index(special)+21
+    return descs.index(special)+2
 
-def description_to_state(desc: Literal['pass', 'hit', 'stand']):
+def description_to_state(desc: Literal['hit', 'stand']):
     descs = ['hit', 'stand']
     return descs.index(desc)
 
@@ -44,14 +45,14 @@ def process_data(data):
         cur_states = []
 
         cur_player_sum = card_to_index(sample['player_hand'][0])+card_to_index(sample['player_hand'][1])
-        cur_dealer_sum = card_to_index(sample['dealer_hand'][0])
+        dealer_up_card = card_to_index(sample['dealer_hand'][0])
         
         # get turn states and emissions
         for turn in sample['turns']:
             cur_states.append(description_to_state(turn['prev_action']))
             if turn['new_card']:
                 cur_player_sum += card_to_index(turn['new_card'])
-            cur_emissions.append([cur_player_sum, special_to_index('cont')])
+            cur_emissions.append([cur_player_sum, dealer_up_card, special_to_index('cont')])
 
         # get last emission
         special = 'win'
@@ -59,7 +60,7 @@ def process_data(data):
             special = 'lose'
         elif sample['outcome'] == 'draw':
             special = 'draw'
-        cur_emissions[-1][1] = special_to_index(special)
+        cur_emissions[-1][2] = special_to_index(special)
 
         states.extend(cur_states)
         emissions.extend(cur_emissions)
@@ -69,19 +70,46 @@ def process_data(data):
     emissions = np.array(emissions)
     return emissions, states, lengths
 
-mhmm = MultinomialHMM()
+
 emissions, states, lengths = process_data(raw_data)
-mhmm.fit(emissions, states, lengths=lengths)
 
-env = BlackjackEnv()
-predictions = []
-for _ in range(1000):
-    env.reset()
-    cur_emissions = []
-    dealer_sum = card_to_index(env.dealer_hand[0])
-    player_sum = card_to_index(env.player_hand[0]) + card_to_index(env.player_hand[1])
-    cur_emissions.append([player_sum, special_to_index('lose')])
-    
-    predictions.append(mhmm.predict(np.array(cur_emissions)))
+wins = 0
+draws = 0
+for sample in tqdm(raw_data):
+    wins += 1 if (sample['outcome'] == 'player_win' or sample['outcome'] == 'dealer_bust') else 0
+    draws += 1 if sample['outcome'] == 'draw' else 0
+print('win rate (random):', wins/len(raw_data))
+print('draw rate (random):', draws/len(raw_data))
 
-print(np.unique(predictions))
+def test_hmm(N_rounds):
+    mhmm = MultinomialHMM()
+    mhmm.fit(emissions, states, lengths=lengths)
+    env = BlackjackEnv()
+    wins = 0
+    draws = 0
+    for _ in tqdm(range(N_rounds)):
+        env.reset()
+        cur_emissions = []
+        dealer_up_card = card_to_index(env.dealer_hand[0])
+        player_sum = card_to_index(env.player_hand[0])+card_to_index(env.player_hand[1])
+        while not env.game_over:
+            actions = 0
+            total = 0
+            # get every possible card in deck (card counting):
+            for card in env.deck.cards + [env.dealer_hand[1]]:
+                prospective_emissions = cur_emissions + [[player_sum+card_to_index(card), dealer_up_card, special_to_index('win')]]
+                actions += mhmm.predict(np.array(prospective_emissions))[0]
+                total += 1
+            action = 1 if actions / total >= 0.5 else 0
+            # action = 0 if action else 1 # flip action since we predicted lose
+            _, _, _, info = env.step(action)
+            player_sum += card_to_index(env.player_hand[-1])
+            cur_emissions.append([player_sum, dealer_up_card, special_to_index('cont')])
+
+        wins += 1 if (info['result'] == 'player_win' or info['result'] == 'dealer_bust') else 0
+        draws += 1 if info['result'] == 'draw' else 0
+    return wins/N_rounds, draws/N_rounds
+
+winrate, drawrate = test_hmm(10000)
+print('win rate (HMM):', winrate) # 0.384 optimize for win 0.383 optimize for lose
+print('draw rate (HMM):', drawrate) # 0.0495 for win 0.0499 optimize for lose
